f24e8d8a81eb285b93dc9af351b22686d98d3818
Robert DeRouin <76668112+robertjosephderouin@users.noreply.github.com>
2022-06-13 10:20:31 -0700

    Update data.js

--- src/data.js
+++ src/data.js
@@ -3,4 +3,244 @@ export default [
     front: "MVP",
     back: "Minimal Viable Product, when developing a project you want to start with the MVP, get the product launched then iterate on it",
   },
+  {
+    front: "Scope",
+    back: "All the features, screens, integrations, and functionality of an application.",
+  },
+  {
+    front: "PRD",
+    back: "Project requirement document, think of this as the holy bible for the application. If it's not on the PRD, your customer didn’t pay for it and we won’t be making it.",
+  },
+  {
+    front: "Code Stack",
+    back: "All the different types of code used by an application.",
+  },
+  {
+    front: "Application Generation Platform",
+    back: "Crowdbotics is an application generation platform, a unique innovative way to build code that combines the speed and value of No Code solutions while maintaining the scalability, flexibility, and customization of old school development houses. ",
+  },
+  {
+    front: "No Code",
+    back: "Cookie cutter application creation platforms that use proprietary prebuilt, minimally customizable technology solutions.  Perfect for no budget projects but won’t be able to be used for novel technologies or any project that wants to expand, be customized or be owned.",
+  },
+  {
+    front: "Hosting",
+    back: "Applications need to live somewhere, hosting refers to setting software up to live somewhere that is connected to the rest of the internet.  Even applications for internal use would need hosting as they need to connect with each other internally. ",
+  },
+  {
+    front: "Crowdbotics Module",
+    back: "The building block of Crowdbotics platform, Modules are pieces of real RAD stack code that start a feature 50% of the way through.  Then real developers writing in real code can shape, customize and scale those features to suit the individual needs of any client. ",
+  },
+  {
+    front: "Heroku",
+    back: "Heroku is a hosting platform that we use on pro plan accounts.",
+  },
+  {
+    front: "GCP",
+    back: "Google Cloud Platform or GCP is what we use for advanced accounts. A hosting platform.",
+  },
+  {
+    front: "AWS",
+    back: "Amazon Web Services is a collection of services provided by Amazon, which provides tools for application development and resources.",
+  },
+  {
+    front: "BA",
+    back: "Business Analyst attends the kick off meeting, scopes the project, and writes the Project Requirement Document.",
+  },
+  {
+    front: "Design",
+    back: "Designers are assigned to projects to create the Figma screens and bring the clients vision to the screen.",
+  },
+  {
+    front: "PM",
+    back: "Project Manager oversees the development of the project, coordinates both the front end team and the back end team. Interacts with the client directly and keeps everyone on task.",
+  },
+  {
+    front: "CSM",
+    back: "Customer Success Manager is the direct contact to the client during development and ensures the client is being heard in a productive way.",
+  },
+  {
+    front: "RAD Stack",
+    back: "React Native, APIs, and Django.  It is the code stack we utilize for Crowdbotics.  It is the same code stack used by Facebook and Instagram and is a modern framework that offers great power and flexibility in development.",
+  },
+  {
+    front: "Component",
+    back: "The basic building block of React.  Components are often single screens or single features / functionality.  A module is most of the time just a prebuilt component.  ",
+  },
+  {
+    front: "2FA",
+    back: "2 Factor Authentication. A security feature that adds security by having users authenticate through 2 means rather than just one.  Password and text authentication for example.",
+  },
+  {
+    front: "Framework",
+    back: "React Native & Django are both frameworks on JavaScript and Python respectively.  Think of frameworks like launch pads for both code stacks that add more functionality than either language would have had natively.  However at the end of the day it is important to understand it's just JavaScript and Python.",
+  },
+  {
+    front: "API",
+    back: "Application programming interface is anything that allows one computer to talk to another computer.  When you want to connect to another software you use “endpoints” to send data between those computers.  So an API integration example for calendar integaration would be... Our app sends data to Google Calendar and Google Calendar turns that into a function on its end.",
+  },
+  {
+    front: "Java",
+    back: "Java is a back end language that is NOT JavaScript and literally has nothing to do with it, or React. Do not use Java and JavaScript interchangeably. ",
+  },
+  {
+    front: "Python",
+    back: "The base language that Django uses. It is a back end language.",
+  },
+  {
+    front: "JavaScript",
+    back: "The base language that React Native uses.  It is a front end  or back end language. We use it for front end.",
+  },
+  {
+    front: "React Native",
+    back: "React Native is like React, however it is better suited for making applications for both Apple and Android phones simultaneously. Both are frameworks for JavaScript, this is Crowdbotic's front end technology.",
+  },
+  {
+    front: "Django",
+    back: "Django is a server framework of Python.  Python is a back end language.",
+  },
+   {
+    front: "Airdrop",
+    back: "An airdrop is a marketing stunt that involves sending free coins, NFTs or tokens to wallet addresses in order to promote awareness of a new virtual currency.",
+  },
+  {
+    front: "Blockchain",
+    back: "A blockchain is a digital ledger of transactions maintained by a network of computers in a way that makes it difficult to hack or alter. The technology offers a secure way for individuals to deal directly with each other, without an intermediary like a government, bank or other third party. Etherium, ether, and Polygon are examples of blockchains. The network at which transactions live in the decentralized world.",
+  },
+  {
+    front: "Burning",
+    back: "Burning crypto means permanently removing a number of tokens from circulation. This is typically done by transferring the tokens in question to a burn address, i.e. a wallet from which they cannot ever be retrieved. This is often described as destroying tokens. A project burns its tokens to reduce the overall supply. Countries will do this with their currency to prevent inflation.",
+  },
+  {
+    front: "Cross Chain",
+    back: "A cross-chain bridge enables an exchange of information, cryptocurrency or NFTs from one blockchain network to another. It enables the flow of data and tokens across what would otherwise be siloed sets of data on different blockchains. This is very difficult to do and very few projects do this.",
+  },
+  {
+    front: "Dapps",
+    back: "A decentralized application (dapp) is an application built on a decentralized network that combines a smart contract and a frontend user interface. On Ethereum, smart contracts are accessible and transparent – like open APIs – so your dapp can even include a smart contract that someone else has written. If we are building something with Web 3.0 on the blockchain, it will be referred to as a Dapp.",
+  },
+  {
+    front: "DeFi",
+    back: "DeFi (pronounced dee-fye) is short for decentralized finance. It's an umbrella term for the part of the crypto universe that is geared toward building a new, internet-native financial system, using blockchains to replace traditional intermediaries and trust mechanisms. A lot of our customers are developing DeFi tools with us.",
+  },
+  {
+    front: "Ether",
+    back: "Ether was the first real blockchain that was launched. Bitcoin is traded on Ether. Ether is the fuel used to power the network, whereas Ethereum refers to the network transactional token that facilitates operations on the Ethereum network. All of the programs and services linked with the Ethereum network require computing power (and that computing power is not free). Ether is a form of payment for network participants to execute their requested operations on the network.",
+  },
+  {
+    front: "Gas",
+    back: "Gas is the fee you pay in order to perform a transaction. On proof of stake networks, such as avalanche, that fee is based on supply & demand only and the fee that you pay is destroyed. Proof of Work networks, such as Ether, have fees that you pay to the hardware miners to process the transaction for you on their nodes.",
+  },
+  {
+    front: "GWEI",
+    back: "It's the baseline transaction cost of doing an action on the blockchain. It's the level at which you pay gas on. It is on all networks to show how much each transaction will cost. It varies on supply & demand. It's smart to time these out to ensure that your clients don't get destroyed by fees.",
+  },
+  {
+    front: "MetaMask v Wallet Connect",
+    back: "MetaMask is any connected wallet that exists (i.e. polygon wallet, treasure wallet, thousands of others) for the purpose of holding your money. Wallet Connect is simply a tool to connect gaps to these various wallets. We just finished a module for wallet connect that will allow us to connect any of our Dapps to any wallet.",
+  },
+  {
+    front: "Metaverse v. Web 3.0",
+    back: "Web 3.0 is a generic term for the next generation of the internet (living on the blockchain, living in a decentralized world without ownership, you'd sign in with a crypto wallet rather than username/password, etc.). Metaverse is also Web 3.0, but it focuses more on the high-level interaction/gaming portion of it. (i.e. Sandbox).",
+  },
+   {
+    front: "Mint",
+    back: "The creation of a token, coin, NFT. It is the process at which it is generated.",
+  },
+   {
+    front: "NFT",
+    back: "Non-Fungible Token. A representation of something (not necessarily a coin or token), it is unique and can not be copied. NFTs can be pictures, music, gifs, and soon videos.",
+  },
+   {
+    front: "Opensea",
+    back: "This is where NFTs can be viewed, traded, offered, bought, and sold in your wallet. You can also create NFTs in Opensea.",
+  },
+   {
+    front: "Play to Earn",
+    back: "The gaming aspect, where you play a game to earn crypto. Axie infinity is the biggest of big as far as play to earn companies.",
+  },
+   {
+    front: "Proof of Work vs. Proof of Stake",
+    back: "Proof of Work is collateralized in power, energy, internet speed, data storage, etc. Proof of stake is collateralized in some sort of an asset (i.e. money), as opposed to power.",
+  },
+   {
+    front: "Rug Pull",
+    back: "A word for extreme theft. We have started some projects here that are specifically wanting us to build rug pull apps, where they can collect money, launch, and then pull the rug out from everybody and leave. It is extremely difficult to catch those individuals.",
+  },
+  {
+    front: "Seed Phrase",
+    back: "The indentifier to your crypto wallet, not the address, but the way you gain access. A seed phrase is normally a series of typically 10-15 characters. If you lose your seed phrase, you don't have a way to get back into your wallet.",
+  },
+   {
+    front: "Smart Contract",
+    back: "A smart contract is required to program NFTs, Dapps, Coins, etc. A smart contract lays out what the programs will do - where the they go, how they interact, etc. This is extremely vital in the development world. Most projects in the Web 3.0 world can not exist without a smart contract. A smart contract itself isn't expensive - it's the integration & complexities that are.",
+  },
+   {
+    front: "Stable Coins - Backed v. Algorithmic",
+    back: "A stable coin is a coin that is pegged to something else (i.e. USD, gold, etc.) - USD is always equal to one US dollar. UST are stable coins that are backed by the USD (meaning, that if you buy one, they buy a USD and your UST is equivalent to that). Algorithmic value is based on a series of buys and sells that happen in the background.",
+  },
+   {
+    front: "Staking",
+    back: "Putting one of your assets on the blockchain in order to support or earn. If you are trying to help a platform succeed, you can stake collateral and later be rewarded.",
+  },
+   {
+    front: "Tokens v. Utility Tokens vs. Coins",
+    back: "Utility token is a token or NFT that performs an actual function. It is a program that earns or takes care of something. A regular token has zero utility. It is created from nothing and there is a limited supply. Their value is only based on supply & demand. Coins are a more advanced version of the token.",
+  },
+   {
+    front: "Whitelist",
+    back: "Also referred to as WL, refers to a list you can be put on to purchase projects before everyone else. It is similar to an ICO, ITO, or IPO in the investing world. People find whitelists and become rich by staying on top of research in Discord.",
+  },
+   {
+    front: "White or Lite Paper",
+    back: "A way that a project will present what it does. Typically 50-100 pages long. Basically a very advanced business plan for a project.",
+  },
+   {
+    front: "Medium",
+    back: "Similar to a blog or newsletter, where updates are posted about projects by the creators. Medium articles are a great way to learn about what a project is, what it is hoping to accomplish, what is coming, etc.",
+  },
+   {
+    front: "ITO or ICO",
+    back: "Initial Token Offering, Initial Coin Offering, when a token or coin launches on the blockchain so that everyone can start buying them.",
+  },
+   {
+    front: "Pools vs. Farms",
+    back: "A farm is a proof of stake. A pool is where people put all of their money together to collect on a utility token or investment into platforms.",
+  },
+   {
+    front: "KYC",
+    back: "Know Your Customer.",
+  },
+   {
+    front: "Onramp v. Offramp",
+    back: "The process of bringing money onto the blockchain and removing it from the blockchain and turning into fiat (regular money)",
+  },
+   {
+    front: "Satoshi Nakamoto",
+    back: "The supposed creator of Bitcoin. Created it after the 2008 financial crash to create an inflation-proof worldwide currency.",
+  },
+   {
+    front: "Dex",
+    back: "Decentralized Exchange. Basically a place at which you can view the values, trade coins/tokens, see the various candlesticks, etc.",
+  },
+   {
+    front: "Coinbase",
+    back: "The largest cryto transaction and blockchain transaction company.",
+  },
+   {
+    front: "TXN",
+    back: "Transaction on the blockchain.",
+  },
+   {
+    front: "FDV v. Market Cap",
+    back: "Fully Diluted Value v. Market Cap. If you sell every single token in its existence at its current price, FDV is the price. Market Cap is the amount of tokens/coins that exist so far, based on what is being transacted.",
+  },
+   {
+    front: "Discord v. Telegram v. Twitter in the Cryptoverse",
+    back: "Communicative platforms for Web 3.0",
+  },
+   {
+    front: "IOT",
+    back: "IoT enables devices across the Internet to send data to private blockchain networks to create tamper-resistant records of shared transactions",
+  },
 ];
