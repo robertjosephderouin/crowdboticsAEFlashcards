83195663a83dd8e0faeece9690106c46d753764d
robertjosephderouin <robertjosephderouin@gmail.com>
2023-01-25 17:18:13 -0700

    added footer

--- src/data.js
+++ src/data.js
@@ -1,6 +1,38 @@
 export default [
   {
     front: "PMC",
-    back: "Property Management Company",
+    back: "A property management company takes on some responsibilities for property owners. This third-party corporation helps maintain the status quo of a residence and handles any issues for its occupants. As a result, real estate owners and investors often hire them to oversee their real estate operations on their behalf.",
+  },
+  {
+    front: "CAM",
+    back: "Common area maintenance (CAM) is the cost your business pays for the areas in a commercial building that are common to all tenants. Look at it this way: When you lease a commercial building, you are paying for two separate areas, the usable area and the common area."
+  },
+  {
+    front:"NNN",
+    back:"A Net Net Net lease is the most common type of commercial lease and is commonly called a triple net lease. On an NNN lease, tenants pay additional expenses in addition to the lease fee, to the landlord or lessor. The NNN fees includes property taxes, property insurance and common area maintenance for a building (CAM)."
+  },
+  {
+    front:"Pro-Rata Share",
+    back:"Pro-rata share is an accounting term used to express sharing within equal proportions. The term itself originates from Latin, and it describes proportionate amounts of something that's being shared, depending on ownership percentages/stakes."
+  },
+  {
+    front:"Leakage",
+    back:"Expenses that are not going to be reimbursed by your tenants."
+  },
+  {
+    front:"CAM Reconciliation",
+    back:"CAM reconciliation is the process landlords use to evaluate whether the amount a tenant paid in estimated escrow charges was correct. This sometimes results in a landlord reimbursing a tenant for overcharges, but more often the tenant has to make up the difference."
+  },
+  {
+    front:"",
+    back:""
+  },
+  {
+    front:"",
+    back:""
+  },
+  {
+    front:"",
+    back:""
   },
 ];
